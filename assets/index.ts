a99d08391a8fcfbfca911fabcdba4145549c36ea
Shafnaa <saujanashafi2003@gmail.com>
2024-01-28 23:07:32 +0700

    feat: adding social media logos

--- assets/index.ts
+++ assets/index.ts
@@ -1,3 +1,15 @@
 import OfficialLogo from "./OfficialLogo";
+import InstagramLogo from "./InstagramLogo";
+import LinkedinLogo from "./LinkedinLogo";
+import SpotifyLogo from "./SpotifyLogo";
+import TiktokLogo from "./TiktokLogo";
+import YoutubeLogo from "./YoutubeLogo";
 
-export { OfficialLogo };
+export {
+  OfficialLogo,
+  InstagramLogo,
+  LinkedinLogo,
+  SpotifyLogo,
+  TiktokLogo,
+  YoutubeLogo,
+};
