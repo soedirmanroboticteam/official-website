f366c3f4704a34e957aac250d65415070fbadb57
Shafnaa <saujanashafi2003@gmail.com>
2024-01-29 01:02:34 +0700

    feat: mobile responsive footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -77,11 +77,11 @@ const footerLinks: {
 
 const Footer = () => {
   return (
-    <footer className="bg-footer w-full">
+    <footer className="bg-footer text-footer-foreground w-full px-4">
       <div className="max-w-screen-xl mx-auto flex flex-col py-8 gap-4">
-        <div className="flex justify-between gap-x-32">
-          <div className="max-w-md flex flex-col gap-6 items-start">
-            <Link href="/" className="flex items-center gap-2 text-foreground">
+        <div className="flex justify-between gap-x-32 gap-y-4 flex-wrap">
+          <div className="max-w-md flex flex-col gap-6 items-center">
+            <Link href="/" className="flex items-center gap-2">
               <OfficialLogo height="64" />
               <h1 className="text-xl font-semibold leading-none">
                 Soedirman <br />
@@ -89,7 +89,7 @@ const Footer = () => {
                 Team
               </h1>
             </Link>
-            <p className="text-justify">
+            <p className=" text-center md:text-justify">
               Ristek is a student-founded non-profit organization from
               Universitas Ristek is a student-founded non-profit organization
               from Universitas Ristek is a student-founded non-profit
@@ -102,10 +102,10 @@ const Footer = () => {
               soedirmanrobotic@gmail.com
             </Link>
           </div>
-          <div className="flex flex-1 justify-between flex-wrap">
+          <div className="flex flex-1 justify-between flex-wrap gap-y-4">
             {footerLinks.map((section, index) => (
               <div className="flex flex-col gap-3" key={index}>
-                <h3 className="font-bold">{section.title}</h3>
+                <h3 className="text-lg font-bold">{section.title}</h3>
                 <ul className="flex flex-col gap-2">
                   {section.contents.map((content, id) => (
                     <li key={id}>
@@ -118,7 +118,7 @@ const Footer = () => {
           </div>
         </div>
         <Separator />
-        <div className="flex justify-between">
+        <div className="flex justify-center md:justify-between flex-wrap-reverse gap-y-4 items-center">
           <p>© 2024 Soedirman Robotic Team</p>
           <div className="flex gap-6 items-center">
             <Link href="https://www.instagram.com/srtunsoed/">
