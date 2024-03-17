fe2e2a950bf44676db3d6a6c45078fa61aab4982
Shafnaa <saujanashafi2003@gmail.com>
2024-03-17 17:01:05 +0700

    feat: working on Team Card

---
+++ components/TeamCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+const TeamCard = ({
+  logo,
+  title,
+  desc,
+}: {
+  logo: React.JSX.Element;
+  title: string;
+  desc: string;
+}) => {
+  return (
+    <div className="w-full h-72 flex odd:flex-row even:flex-row-reverse gap-24">
+      <Card className="w-fit h-full">
+        <CardHeader>
+          <div className="w-fit">{logo}</div>
+          <CardTitle className="pt-8">{title}</CardTitle>
+          <CardDescription>{desc}</CardDescription>
+        </CardHeader>
+      </Card>
+    </div>
+  );
+};
+
+export default TeamCard;
