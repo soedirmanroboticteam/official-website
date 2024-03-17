ffa2dacc78dc74fea2a61b84473eb930da82c3a8
Shafnaa <saujanashafi2003@gmail.com>
2024-03-17 17:02:10 +0700

    fix: displaying hero & team section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 // import Image from "next/image";
 
+import HeroSection from "@/components/HeroSection";
+import TeamSection from "@/components/TeamSection";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between px-12">
+      <HeroSection />
+      <TeamSection />
+    </main>
   );
 }
