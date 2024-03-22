a50afc3586c7f22b026389d54a48a391d9056fc6
Shafnaa <saujanashafi2003@gmail.com>
2024-03-22 17:15:23 +0700

    fix: fixing lib

--- lib/contactSection.ts
+++ lib/contactSection.ts
@@ -3,86 +3,185 @@ import {
   h1a021077,
   h1a022026,
   h1a022020,
-  h1a023024,
-  b1a022001,
   h1a023101,
+  b1a022001,
+  h1a023024,
   h1e023009,
-} from "@/assets/management/";
+} from "@/assets/management/bph";
+import {
+  h1a021030,
+  h1a022039,
+  h1a022068,
+  h1a023048,
+  h1a023095,
+  h1a023124,
+  h1e023087,
+} from "@/assets/management/hrd";
+import {
+  b1a022153,
+  b1a023238,
+  f1c023058,
+  h1a021053,
+  h1e021037,
+  k1c023053,
+} from "@/assets/management/medcre";
+import {
+  a1d022159,
+  f1b023010,
+  i1a023034,
+  j1d021038,
+  k1a022068,
+  l1b022026,
+} from "@/assets/management/pr";
+import {
+  h1a022010,
+  h1a022016,
+  h1a022059,
+  h1a023022,
+  h1a023034,
+  k1c023055,
+  k1c023068,
+} from "@/assets/management/sponsorship";
 import { StaticImageData } from "next/image";
 
 export const contactImages: {
+  alt: string;
   img: StaticImageData;
 }[] = [
   {
+    alt: "President",
     img: h1a021040,
   },
   {
+    alt: "Vice President",
     img: h1a021077,
   },
   {
+    alt: "Head of Secretary",
     img: h1a022026,
   },
   {
+    alt: "Secretary",
     img: h1a022020,
   },
   {
+    alt: "Secretary",
     img: h1a023101,
   },
   {
+    alt: "Head of Financial Manager",
     img: b1a022001,
   },
   {
+    alt: "Financial Manager",
     img: h1a023024,
   },
   {
+    alt: "Financial Manager",
     img: h1e023009,
   },
   {
-    img: h1a021040,
+    alt: "Head of Human Resource Development",
+    img: h1a022068,
   },
   {
-    img: h1a021077,
+    alt: "Human Resource Development",
+    img: h1a021030,
   },
   {
-    img: h1a022026,
+    alt: "Human Resource Development",
+    img: h1a023048,
   },
   {
-    img: h1a022020,
+    alt: "Human Resource Development",
+    img: h1e023087,
   },
   {
-    img: h1a023101,
+    alt: "Human Resource Development",
+    img: h1a023124,
   },
   {
-    img: b1a022001,
+    alt: "Human Resource Development",
+    img: h1a022039,
   },
   {
-    img: h1a023024,
+    alt: "Human Resource Development",
+    img: h1a023095,
   },
   {
-    img: h1e023009,
+    alt: "Head of Public Relation",
+    img: j1d021038,
   },
   {
-    img: h1a021040,
+    alt: "Public Relation",
+    img: f1b023010,
   },
   {
-    img: h1a021077,
+    alt: "Public Relation",
+    img: l1b022026,
   },
   {
-    img: h1a022026,
+    alt: "Public Relation",
+    img: i1a023034,
   },
   {
-    img: h1a022020,
+    alt: "Public Relation",
+    img: a1d022159,
   },
   {
-    img: h1a023101,
+    alt: "Public Relation",
+    img: k1a022068,
   },
   {
-    img: b1a022001,
+    alt: "Head of Creative Media",
+    img: h1a021053,
   },
   {
-    img: h1a023024,
+    alt: "Creative Media",
+    img: k1c023053,
   },
   {
-    img: h1e023009,
+    alt: "Creative Media",
+    img: b1a022153,
+  },
+  {
+    alt: "Creative Media",
+    img: b1a023238,
+  },
+  {
+    alt: "Creative Media",
+    img: f1c023058,
+  },
+  {
+    alt: "Creative Media",
+    img: h1e021037,
+  },
+  {
+    alt: "Head of Sponsorship",
+    img: h1a022010,
+  },
+  {
+    alt: "Sponsorship",
+    img: k1c023068,
+  },
+  {
+    alt: "Sponsorship",
+    img: k1c023055,
+  },
+  {
+    alt: "Sponsorship",
+    img: h1a022059,
+  },
+  {
+    alt: "Sponsorship",
+    img: h1a023022,
+  },
+  {
+    alt: "Sponsorship",
+    img: h1a023034,
+  },
+  {
+    alt: "Sponsorship",
+    img: h1a022016,
   },
 ];
--- lib/ourTeam.ts
+++ lib/ourTeam.ts
@@ -5,6 +5,7 @@ import {
   biantaraLogo,
   StaticToJsx,
 } from "@/assets";
+import { StaticImageData } from "next/image";
 
 export const teamSection: {
   title: string;
@@ -18,16 +19,25 @@ export const ourTeams: {
   title: string;
   desc: string;
   logo: React.JSX.Element;
+  direction: "forward" | "backward";
+  members: {
+    alt: string;
+    img: StaticImageData;
+  }[];
 }[] = [
   {
     title: "Blakasutha",
     desc: "Our representative for the Kontes Robot ABU Indonesia (KRAI) division of Kontes Robot Indonesia (KRI).",
     logo: BlakasuthaLogo({ height: 40 }),
+    direction: "forward",
+    members: [],
   },
   {
     title: "Satria",
     desc: "Our representative for the Kontes Robot SAR Indonesia (KRSRI) division of Kontes Robot Indonesia (KRI).",
     logo: StaticToJsx({ src: satriaLogo, alt: "Satria Logo", height: 40 }),
+    direction: "backward",
+    members: [],
   },
   {
     title: "Yudishtira",
@@ -37,15 +47,21 @@ export const ourTeams: {
       alt: "Yudishtira Logo",
       height: 40,
     }),
+    direction: "forward",
+    members: [],
   },
   {
     title: "Biantara Racing Plane",
     desc: "Our representative for the Racing Plane (RP) division of Kontes Robot Terbang Indonesia (KRTI).",
     logo: StaticToJsx({ src: biantaraLogo, alt: "Biantara Logo", height: 40 }),
+    direction: "backward",
+    members: [],
   },
   {
     title: "Biantara Fixed Wing",
     desc: "Our representative for the Fixed Wing (FW) division of Kontes Robot Terbang Indonesia (KRTI).",
     logo: StaticToJsx({ src: biantaraLogo, alt: "Biantara Logo", height: 40 }),
+    direction: "forward",
+    members: [],
   },
 ];
