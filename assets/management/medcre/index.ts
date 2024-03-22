868437e8d408adb2c0999229f892c908e09f9c2c
Shafnaa <saujanashafi2003@gmail.com>
2024-03-22 17:13:08 +0700

    refactor: adding management photos

---
+++ assets/management/medcre/index.ts
@@ -0,0 +1,15 @@
+import h1a021053 from "./h1a021053.png";
+import h1e021037 from "./h1e021037.png";
+import f1c023058 from "./f1c023058.png";
+import k1c023053 from "./k1c023053.png";
+import b1a023238 from "./b1a023238.png";
+import b1a022153 from "./b1a022153.png";
+
+export {
+  h1a021053,
+  h1e021037,
+  f1c023058,
+  k1c023053,
+  b1a023238,
+  b1a022153,
+};
---
+++ assets/management/pr/index.ts
@@ -0,0 +1,8 @@
+import j1d021038 from "./j1d021038.png";
+import l1b022026 from "./l1b022026.png";
+import k1a022068 from "./k1a022068.png";
+import a1d022159 from "./a1d022159.png";
+import f1b023010 from "./f1b023010.png";
+import i1a023034 from "./i1a023034.png";
+
+export { j1d021038, l1b022026, k1a022068, a1d022159, f1b023010, i1a023034 };
