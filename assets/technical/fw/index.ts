589802e306d7b190191e7b7de0c972e5eef7cf20
Shafnaa <saujanashafi2003@gmail.com>
2024-03-23 19:06:56 +0700

    fix: remove duplicate & adding export

---
+++ assets/technical/fw/index.ts
@@ -0,0 +1,27 @@
+import fw01 from "./fw01.png";
+import fw02 from "./fw02.png";
+import fw03 from "./fw03.png";
+import fw04 from "./fw04.png";
+import fw05 from "./fw05.png";
+import fw06 from "./fw06.png";
+import fw07 from "./fw07.png";
+import fw08 from "./fw08.png";
+import fw09 from "./fw09.png";
+import fw10 from "./fw10.png";
+import fw11 from "./fw11.png";
+import fw12 from "./fw12.png";
+
+export {
+  fw01,
+  fw02,
+  fw03,
+  fw04,
+  fw05,
+  fw06,
+  fw07,
+  fw08,
+  fw09,
+  fw10,
+  fw11,
+  fw12,
+};
