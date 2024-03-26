e2effed1b4ad49edd658ac9e6e49c63446c6e7fe
Shafnaa <saujanashafi2003@gmail.com>
2024-03-26 16:09:23 +0700

    feat: hero section mobile

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,31 +5,31 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <div className="w-full flex-col justify-start items-center flex">
-      <div className="self-stretch py-12 pl-24 bg-black justify-center items-center gap-32 flex">
-        <div className=" h-full">
-          <OfficialLogo height={360} />
+    <div className="w-full flex-col justify-start items-center py-6 md:py-12 flex">
+      <div className="relative flex-col md:flex-row bg-black justify-center items-center gap-8 md:gap-32 flex">
+        <div className="relative h-60 md:h-80">
+          <OfficialLogo height={"100%"} />
         </div>
-        <div className="flex-col justify-start items-start flex">
-          <div className="justify-center items-start inline-flex">
-            <div className="self-stretch flex-col justify-start items-center inline-flex">
-              <div className="text-center text-foreground text-xl font-normal font-poppins leading-7">
+        <div className="relative flex-col justify-start items-start flex">
+          <div className="justify-center items-start flex">
+            <div className="self-stretch flex-col justify-start items-center flex">
+              <div className="text-center text-foreground text-xs md:text-xl font-normal font-poppins md:leading-7">
                 Universitas Jenderal Soedirman
               </div>
             </div>
           </div>
-          <div className="self-stretch justify-center items-start inline-flex">
+          <div className="self-stretch justify-center items-start flex">
             <div className="w-full self-stretch py-1 justify-center items-center flex">
-              <div className="text-white text-8xl font-bold font-poppins uppercase ">
+              <div className="text-white text-5xl md:text-8xl font-bold font-poppins uppercase leading-10">
                 Soedirman <br />
                 Robotic <br />
                 Team
               </div>
             </div>
           </div>
-          <div className="justify-center items-start inline-flex">
-            <div className="self-stretch flex-col justify-start items-center inline-flex">
-              <div className="text-center text-foreground text-xl font-normal font-poppins italic leading-7">
+          <div className="justify-center items-start flex">
+            <div className="self-stretch flex-col justify-start items-center flex">
+              <div className="text-center text-foreground text-xs md:text-xl font-normal font-poppins italic md:leading-7">
                 “The Future We Make, For The Better Life!”
               </div>
             </div>
