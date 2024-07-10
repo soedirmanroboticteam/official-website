e2a89977b3658343ae754f5b34274cd968d66821
Aldorino Muharram <arru.zq@gmail.com>
2024-07-10 14:48:15 +0700

    add text sponsorship

--- app/components/SponsorSection.tsx
+++ app/components/SponsorSection.tsx
@@ -12,6 +12,17 @@ const SponsorSection = async () => {
     <section
       className={`${sora.className} w-full flex-col justify-start items-start flex`}
     >
+      <div className="grow shrink basis-0 self-stretch px-6 md:px-14 flex-col justify-center items-center flex">
+        <div className="pb-5 flex-col justify-start items-start flex">
+          <div className="h-8 px-2 justify-start items-start flex">
+            <div className="flex-col justify-start items-start flex">
+              <div className="text-2xl font-semibold leading-loose">
+                Our Achievements
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="grow shrink basis-0 self-stretch px-14 flex-col justify-center items-center flex">
         <div className="w-full overflow-hidden">
           <div className="flex overflow-hidden w-full justify-start items-start">
