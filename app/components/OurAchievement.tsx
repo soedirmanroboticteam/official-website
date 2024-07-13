01d565915d6ed86263d314c4ab5c4872c680aa35
Aldorino Muharram <arru.zq@gmail.com>
2024-07-14 00:32:58 +0700

    Commit m- "Fixing Font Size & Text"

--- app/components/OurAchievement.tsx
+++ app/components/OurAchievement.tsx
@@ -13,8 +13,11 @@ const OurAchievement = () => {
         </div>
       <div className="w-full md:w-1/2 px-10 md:px-32">
         <div className="text-xl">2021 - 2024</div>
-        <div className="md:text-3xl text-xl  font-bold mb-4">Our History</div>
-        <div className="text-xl text-justify">
+        <div className="md:text-3xl text-2xl  font-bold mb-4">
+          Pencapaian <br />
+          SRT <br />
+        </div>
+        <div className="md:text-xl text-lg text-justify">
             Soedirman Robotic Team telah meraih berbagai prestasi seperti juara tiga nasional divisi racing plane pada tahun 2023, peringkat 5 nasional divisi KRSRI tahun 2021, dan masih banyak lagi. Saat ini,  Tim Blakasutha KRAI dan Tim Satria KRSRI telah melaksanakan Kontes Robot Indonesia 2024 pada tanggal 1 - 6 Juli 2024 di
         </div>
       </div>
--- app/components/OurPlan.tsx
+++ app/components/OurPlan.tsx
@@ -7,8 +7,11 @@ const OurHistory = () => {
     <section className="flex flex-col md:flex-row justify-between items-center gap-8 py-6 md:py-12">
       <div className="w-full md:w-1/2 px-10 md:px-32">
         <div className="text-xl">2024 - Now</div>
-        <div className="md:text-3xl text-xl  font-bold mb-4">Our History</div>
-        <div className="text-xl text-justify">
+        <div className="md:text-3xl text-2xl  font-bold mb-4">
+            Progress Ke <br />
+            Depan <br />
+        </div>
+        <div className="md:text-xl text-lg text-justify">
             Soedirman Robotic Team bergerak dalam pengembangan ilmu robotika dan keorganisasian di Universitas Jenderal Soedirman. Kegiatan kedepan yang akan dilaksanakan oleh UKM ini berupa kegiatan magang yang dapat diikuti dengan mengisi form pada <a href="https://www.soedirmanrobotic.com/internship" className="text-yellow-600 underline" > link </a>ini 
         </div>
       </div>
