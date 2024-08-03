80eec093f04af302f3f25255dd79029288a37160
Shafnaa <saujanashafi2003@gmail.com>
2024-08-03 15:40:35 +0700

    style: Profile Picture about page

--- components/DivisionSection.tsx
+++ components/DivisionSection.tsx
@@ -28,17 +28,19 @@ const DivisionSection = async ({
     .order("title", { ascending: true });
 
   return (
-    <section className="w-3/4 flex flex-wrap md:flex-row justify-center items-center py-6">
+    <section className="w-full px-2 md:px-12 py-6">
       <div className="w-full text-center font-bold text-2xl md:text-4xl font-poppins pb-8">
         {division.name}
       </div>
-      {data && data.length ? (
-        data.map((member, index) => (
-          <ProfilePicture member={member} key={index} />
-        ))
-      ) : (
-        <div>No data</div>
-      )}
+      <div className="flex flex-row justify-center gap-4 flex-wrap">
+        {data && data.length ? (
+          data.map((member, index) => (
+            <ProfilePicture member={member} key={index} />
+          ))
+        ) : (
+          <div>No data</div>
+        )}
+      </div>
     </section>
   );
 };
