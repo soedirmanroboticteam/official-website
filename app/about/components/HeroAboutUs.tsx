a1024f7d118d82e1551a376d086a4f1c1bfb25c2
Shafnaa <saujanashafi2003@gmail.com>
2024-08-16 19:07:48 +0700

    fix: fix minor class typo

--- app/about/components/HeroAboutUs.tsx
+++ app/about/components/HeroAboutUs.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
-'use client' //Client Side
+"use client"; //Client Side
 import { other01 } from "@/assets/otherpages";
-import Image from 'next/image';
+import Image from "next/image";
 
 const HeroAboutUs = () => {
   return (
     <section className="flex-col justify-start items-center gap-8 py-6 md:py-12 flex">
-      <div className="relative flex flex-col md:flex-row bg-black justify-center items-center gap-8 md:gap-32 w-full">
+      <div className="relative flex flex-col md:flex-row justify-center items-center gap-8 md:gap-32 w-full">
         <div className="relative flex flex-col justify-center items-center">
           <div className="w-full py-1 flex justify-center items-center">
             <div className="text-white text-center text-5xl md:text-8xl font-bold font-poppins uppercase leading-10">
@@ -22,9 +22,9 @@ const HeroAboutUs = () => {
           </div>
           <div className="w-full flex justify-center items-center mt-8 md:max-w-7xl max-w-6xl md:px-10 px-4">
             <Image
-                src={other01}
-                alt="Team Image"
-                className="border-20px border-black rounded-lg bg-black "
+              src={other01}
+              alt="Team Image"
+              className="border-20px border-black rounded-lg bg-black "
             />
           </div>
           <div className="w-full flex justify-center items-center text-center mt-10 md:mt-24">
