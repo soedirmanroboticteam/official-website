166e76dfb22ecb7e753cfe473e930e5e611be2c8
Shafnaa <saujanashafi2003@gmail.com>
2024-08-17 14:24:39 +0700

    feat: coming soon progress bar

--- app/internship/page.tsx
+++ app/internship/page.tsx
@@ -2,7 +2,7 @@ import { createClientBrowserServer } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
-import FormTab from "./components/FormTab";
+import FormTab from "@/app/internship/components/FormTab";
 import SectionTitle from "@/components/SectionTitle";
 import { Progress } from "@/components/ui/progress";
 
@@ -28,17 +28,15 @@ interface OptionsInterface {
 }
 
 export default async function ProtectedPage() {
-  const supabase = createClientBrowserServer();
+  const date = {
+    coming: new Date("2024-08-11 00:00:00.000000+00"),
+    debug: new Date("2024-08-18 00:00:00.000000+00"),
+    start: new Date("2024-08-26 00:00:00.000000+00"),
+    end: new Date("2024-08-29 00:00:00.000000+00"),
+    extend: new Date("2024-08-17 00:00:00.000000+00"),
+  };
 
-  const { data, error } = await supabase.auth.getUser();
-
-  if (error || !data?.user) {
-    redirect("/login");
-  }
-
-  test: if (new Date("2024-08-26 00:00:00.000000+00").getTime() > Date.now()) {
-    break test;
-
+  if (date.debug.getTime() < Date.now() && date.start.getTime() > Date.now()) {
     return (
       <main className="container mx-auto py-4">
         <SectionTitle
@@ -47,16 +45,42 @@ export default async function ProtectedPage() {
         />
         <Progress
           value={
-            ((new Date("2024-08-11 00:00:00.000000+00").getTime() -
-              Date.now()) /
-              (new Date("2024-08-11 00:00:00.000000+00").getTime() -
-                new Date("2024-08-26 00:00:00.000000+00").getTime())) *
+            ((date.coming.getTime() - Date.now()) /
+              (date.coming.getTime() - date.start.getTime())) *
             100
           }
         />
       </main>
     );
   }
+
+  const supabase = createClientBrowserServer();
+
+  const count = await supabase.from("intern_applications").select("");
+
+  if (count.error) {
+    return <div>Error: {count.error.message}</div>;
+  }
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+
+  if (Date.now() > date.extend.getTime())
+    return (
+      <main className="container mx-auto py-4 space-y-4">
+        <SectionTitle
+          title="Closed Registration"
+          desc="Thank you for your participation! Now that the registration is closed. For those that have filled our form, you'll be contacted soon. Further information will be provided there."
+        />
+        <h3 className="text-center text-8xl font-semibold">
+          {count.data.length} <br />
+        </h3>
+        <h4 className="text-center text-3xl font-semibold">registrant</h4>
+      </main>
+    );
 
   const [majors, years, options] = await Promise.all([
     supabase
@@ -89,16 +113,18 @@ export default async function ProtectedPage() {
 
   return (
     <main className="w-full flex flex-col gap-20 items-center justify-center p-4">
-      <section className="w-full max-w-3xl">
-        <Alert variant="destructive">
-          <ExclamationTriangleIcon className="h-4 w-4" />
-          <AlertTitle>Warning!!!</AlertTitle>
-          <AlertDescription>
-            This form is still under development! All the submitted data will be
-            deleted until the official Open Internship is announced.
-          </AlertDescription>
-        </Alert>
-      </section>
+      {date.debug.getTime() < Date.now() ? null : (
+        <section className="w-full max-w-3xl">
+          <Alert variant="destructive">
+            <ExclamationTriangleIcon className="h-4 w-4" />
+            <AlertTitle>Warning!!!</AlertTitle>
+            <AlertDescription>
+              This form is still under development! All the submitted data will
+              be deleted until the official Open Internship is announced.
+            </AlertDescription>
+          </Alert>
+        </section>
+      )}
 
       <FormTab
         userId={data.user.id}
@@ -108,15 +134,34 @@ export default async function ProtectedPage() {
       />
 
       <section className="w-full max-w-3xl">
-        <Alert variant="destructive">
-          <ExclamationTriangleIcon className="h-4 w-4" />
-          <AlertTitle>Warning!!!</AlertTitle>
-          <AlertDescription>
-            This form is still under development! All the submitted data will be
-            deleted until the official Open Internship is announced.
-          </AlertDescription>
-        </Alert>
+        <Progress
+          value={
+            Date.now() > date.end.getTime()
+              ? ((date.end.getTime() - Date.now()) /
+                  (date.end.getTime() - date.extend.getTime())) *
+                100
+              : ((date.start.getTime() - Date.now()) /
+                  (date.start.getTime() - date.end.getTime())) *
+                100
+          }
+          className={`${
+            Date.now() > date.end.getTime() ? "bg-destructive/50" : ""
+          }`}
+        />
       </section>
+
+      {date.debug.getTime() < Date.now() ? null : (
+        <section className="w-full max-w-3xl">
+          <Alert variant="destructive">
+            <ExclamationTriangleIcon className="h-4 w-4" />
+            <AlertTitle>Warning!!!</AlertTitle>
+            <AlertDescription>
+              This form is still under development! All the submitted data will
+              be deleted until the official Open Internship is announced.
+            </AlertDescription>
+          </Alert>
+        </section>
+      )}
     </main>
   );
 }
