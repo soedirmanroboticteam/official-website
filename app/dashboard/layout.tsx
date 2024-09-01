3e2387a3dc3dfed65a8b9b971d2b45f2b8a08923
Shafnaa <saujanashafi2003@gmail.com>
2024-09-02 06:55:59 +0700

    fix: module not found footer

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/footer";
 import NavigationBar from "@/components/navigation-bar";
+import Footer from "@/components/footer";
 
 export default function AppLayout({
   children,
