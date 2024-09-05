854ddbb9e0632c0fa8ae9fa7bf4c2004623532f4
Shafnaa <saujanashafi2003@gmail.com>
2024-09-05 23:37:42 +0700

    fix: client cookieOptions

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -4,7 +4,11 @@ export function createClientBrowserClient() {
   // Create a supabase client on the browser with project's credentials
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookieOptions: {
+        domain: ".soedirmanrobotic.com",
+      },
+    }
   );
 }
-
