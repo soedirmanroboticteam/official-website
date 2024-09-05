685f62ed2319f077fe5015192ce57052a87d9bc1
Shafnaa <saujanashafi2003@gmail.com>
2024-09-05 23:39:50 +0700

    fix: client cookieOptions

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -12,6 +12,9 @@ export async function updateSession(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      cookieOptions: {
+        domain: ".soedirmanrobotic.com",
+      },
       cookies: {
         get(name: string) {
           return request.cookies.get(name)?.value;
