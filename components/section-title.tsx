b6be627884039b1074fd8153ec572460cbfabc50
Shafnaa <saujanashafi2003@gmail.com>
2024-09-09 02:54:47 +0700

    style: fixing overflowing section title

--- components/section-title.tsx
+++ components/section-title.tsx
@@ -2,20 +2,16 @@ import React from "react";
 
 const SectionTitle = ({ title, desc }: { title: string; desc: string }) => {
   return (
-    <div className="flex flex-col md:grid md:grid-flow-col grid-cols-9 w-full py-5 justify-center items-start gap-2 md:gap-5">
+    <div className="flex flex-col md:grid md:grid-flow-col grid-cols-9 w-full py-5 justify-center items-start gap-6 md:gap-5">
       <div className="col-span-4 md:px-2 justify-start items-start flex">
-        <div className="self-stretch pr-12 pb-12 flex-col justify-start items-start inline-flex">
-          <div className="text-foreground text-5xl font-normal leading-10">
-            {title}
-          </div>
-        </div>
+        <h3 className="text-foreground text-5xl font-normal leading-10">
+          {title}
+        </h3>
       </div>
       <div className="col-span-5 w-full md:px-2 justify-start items-start flex">
-        <div className="self-stretch flex-col justify-start items-start inline-flex">
-          <div className="w-full text-justify text-foreground text-opacity-90 text-xl font-normal leading-7">
-            {desc}
-          </div>
-        </div>
+        <p className="w-full text-justify text-foreground text-opacity-90 text-xl font-normal leading-7">
+          {desc}
+        </p>
       </div>
     </div>
   );
